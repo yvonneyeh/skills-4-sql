12a0e93d8c27366560c5e4cc422a9df26efdc661
Yvonne Yeh <yvonneyeh@me.com>
2021-01-30 18:06:02 +0000

    Solve Problem 1 of 6

---
+++ answers.sql
@@ -0,0 +1,25 @@
+1
+
+-----
+
+Balloonicorn's Cupcake shop is getting ready to send a marketing
+email announcing a new promotion on Funfetti cupcakes. We need to get all of
+our customers' email addresses to Marketing.
+ 
+Select all of the email addresses from the Customers table in alphabetical order.
+
+The result set should be:
+        email        
+---------------------
+ aperson@hotmail.com
+ bpersonne@yahoo.com
+ jdoe@gmail.com
+
+
+
+-----
+
+
+SELECT email
+FROM customers
+ORDER BY email;
