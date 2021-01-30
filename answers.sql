242948feb4780741c057614ac5d5be0e8c957616
Yvonne Yeh <yvonneyeh@me.com>
2021-01-30 19:10:04 +0000

    Refactor SQL

--- answers.sql
+++ answers.sql
@@ -5,11 +5,11 @@
 Balloonicorn's Cupcake shop is getting ready to send a marketing
 email announcing a new promotion on Funfetti cupcakes. We need to get all of
 our customers' email addresses to Marketing.
- 
+
 Select all of the email addresses from the Customers table in alphabetical order.
 
 The result set should be:
-        email        
+        email
 ---------------------
  aperson@hotmail.com
  bpersonne@yahoo.com
@@ -20,9 +20,10 @@ The result set should be:
 -----
 
 
-SELECT email
-FROM customers
-ORDER BY email;
+  SELECT  email
+    FROM  customers
+ORDER BY  email
+;
 
 
 ==========
@@ -32,12 +33,12 @@ ORDER BY email;
 
 Jane Doe has requested receipts for all of her past cupcake orders.
 To look up the receipts, we need to know which orders Jane placed.
- 
+
 Write a query that lists the order ids for all orders placed by customers with
 the first name Jane and last name Doe. Use a SUBQUERY to do this, not a join.
 
 The result set should be:
- id 
+ id
 ----
   1
   3
@@ -50,7 +51,15 @@ The result set should be:
 -----
 
 
-SELECT id FROM orders WHERE customer_id = (SELECT id FROM customers WHERE fname = 'Jane' AND lname = 'Doe')
+SELECT  id
+  FROM  orders
+ WHERE  customer_id = (
+          SELECT  id
+            FROM  customers
+           WHERE  fname = 'Jane'
+                  AND
+                  lname = 'Doe'
+        )
 ;
 
 
@@ -61,20 +70,23 @@ SELECT id FROM orders WHERE customer_id = (SELECT id FROM customers WHERE fname 
 
 Balloonicorn needs to schedule her employees for the next week
 based on how many cupcakes have been ordered and not yet processed.
- 
+
 Write a query that gets the total number of cupcakes from unprocessed orders.
 
 The result set should be:
- sum 
+ sum
 -----
  209
  (1 row)
- 
+
 
 -----
 
 
-SELECT SUM(num_cupcakes) FROM orders WHERE processed = 'FALSE';
+SELECT  SUM(num_cupcakes)
+  FROM  orders
+ WHERE  processed = 'FALSE'
+;
 
 
 ==========
@@ -84,7 +96,7 @@ SELECT SUM(num_cupcakes) FROM orders WHERE processed = 'FALSE';
 
 Balloonicorn is working on her inventory and wants to know how many
 cupcakes have been ordered of each type.
- 
+
 Write a query that shows the name of each cupcake and the sum of cupcakes
 ordered for that cupcake type (for both processed and unprocessed orders),
 sorted in ascending alphabetical order of cupcake name.
@@ -93,10 +105,10 @@ The report should show all cupcake types, even if they have not been ordered
 at all.
 
 The result set should be:
-    name     | sum 
+    name     | sum
 -------------+-----
  carrot cake |  10
- chocolate   |    
+ chocolate   |
  funfetti    |  13
  raspberry   |  25
  red velvet  | 215
@@ -107,7 +119,14 @@ The result set should be:
 -----
 
 
-SELECT c.name, SUM(o.num_cupcakes) AS sum FROM cupcakes AS c LEFT JOIN orders AS o ON (c.id = o.cupcake_id) GROUP BY name ORDER BY name;
+   SELECT  c.name,
+           SUM(o.num_cupcakes) AS sum
+     FROM  cupcakes AS c
+LEFT JOIN  orders AS o
+       ON  (c.id = o.cupcake_id)
+ GROUP BY  name
+ ORDER BY  name
+;
 
 
 ==========
@@ -117,13 +136,13 @@ SELECT c.name, SUM(o.num_cupcakes) AS sum FROM cupcakes AS c LEFT JOIN orders AS
 
 Marketing now wants to send emails to all of our top customers
 thanking them for their business.
- 
+
 Write a query that gets the email address of all customers who have ordered
 more than 100 cupcakes total (from both processed and unprocessed orders),
 in reverse alphabetical order.
 
 The result set should be:
-        email        
+        email
 ---------------------
  jdoe@gmail.com
  bpersonne@yahoo.com
@@ -133,7 +152,13 @@ The result set should be:
 -----
 
 
-SELECT c.email FROM customers AS c JOIN orders AS o ON (c.id = o.customer_id) WHERE o.num_cupcakes >= 100 ORDER BY email DESC;
+SELECT  c.email
+  FROM  customers AS c
+  JOIN  orders AS o
+    ON  (c.id = o.customer_id)
+ WHERE  o.num_cupcakes >= 100
+ORDER BY email DESC
+;
 
 
 ==========
@@ -145,13 +170,13 @@ Uh oh -- it looks like there might have been an issue with a recent
 batch of funfetti cupcakes (someone might have ordered non-edible sparkles
 to put in the icing instead of edible ones). We need to let our customers
 who have processed orders of funfetti cupcakes know about the issue.
- 
+
 Write a query that selects the first name, last name and email address of
 customers who have processed orders of funfetti cupcakes. Even if a customer
 has multiple outstanding orders of funfetti, their email should only appear once.
 
 The result set should be:
- fname |  lname   |        email        
+ fname |  lname   |        email
 -------+----------+---------------------
  Bob   | Personne | bpersonne@yahoo.com
 (1 row)
@@ -160,4 +185,17 @@ The result set should be:
 -----
 
 
-SELECT DISTINCT fname, lname, email FROM customers AS c JOIN orders AS o ON (c.id = o.customer_id) WHERE o.cupcake_id = (SELECT id FROM cupcakes WHERE name = 'funfetti') AND processed = true;
+SELECT DISTINCT
+       fname,
+       lname,
+       email
+ FROM  customers AS c
+ JOIN  orders AS o
+   ON  (c.id = o.customer_id)
+ WHERE  o.cupcake_id = (
+        SELECT id
+        FROM cupcakes
+        WHERE name = 'funfetti'
+        ) AND
+        processed = true
+;
